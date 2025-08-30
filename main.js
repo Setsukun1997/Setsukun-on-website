7c025a338d15f02b28097509d0fb4ef4606d4988
Setsukun1997 <tety12308@hotmail.com>
2025-08-30 22:13:34 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventlistener("DOMContentLoaded", () => {
+  const goHomeBtn = document.getElementById("goHomeBtn");
+  if (goHomeBtn) {
+   goHomeBtn.addEventlistener("click", () => {
+   window.location.href = "home.html";
+   if (window.location.pathname.includes("index.html")) {
+   });
+  }
+});
