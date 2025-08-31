f3da0bff4750be9180aae4a188261e434b9a8082
Setsukun1997 <tety12308@hotmail.com>
2025-08-31 13:21:54 +0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,7 +4,6 @@ window.addEventlistener("DOMContentLoaded", () => {
    goHomeBtn.addEventlistener("click", () => {
    window.location.href = "home.html";
    if (window.location.pathname.includes("index.html")) {
-   });
   }
 });
  
