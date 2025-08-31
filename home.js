97841c38ac69866cbfb883b361b2ad3050652cf7
Setsukun1997 <tety12308@hotmail.com>
2025-08-31 13:58:00 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const gotoResumeBtn = document.getElementById("gotoResumeBtn");
+  if (gotoResumeBtn) {
+    gotoResumeBtn.addEventListener("click", () => {
+      window.location.href = "Resume.html";
+window.addEventListener("DOMContentLoaded", ()=> {
+  const gotomyFacebookBtn = document.getElementById("gotomyFacebookBtn");
+  if (gotomyFacebookBtn) {
+    gotomyFacebookBtn.addEventListener('click", () => {
+window.addEventListener(DOMContentLoaded", () => {
+  const gotomyYoutubeBtn = document.getElementById("gotomyyoutubeBtn");
+  if (gotomyYoutubeBtn) {
+    gotomYoutubeBtn.addEventListener("click", () => {
+
+    });
+  }
+});
