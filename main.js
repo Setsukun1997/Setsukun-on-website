67a35cfda90f50b0cf31cd2b7cd2909836b6235b
Setsukun1997 <tety12308@hotmail.com>
2025-08-31 13:16:50 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const goHome = document.getElementById("goHome");
-    if (goHome) {
-        goHome.addEventListener("click", () => {
-            window.location.href = "home.html"; // พาไปหน้า home.html
-        });
-    }
-});
