18e0d3c8a8e894f407619338a78c187f3037b985
Setsukun1997 <tety12308@hotmail.com>
2025-09-05 13:36:47 +0700

    Update register.js

--- register.js
+++ register.js
@@ -2,7 +2,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const gohomeBtn = document.getElementById("gohomeBtn");
   if (gohomeBtn) {
     gohomeBtn.addEventListener("click", () => {
-      window.location.href = "home.html";
+      window.location.href = "index.html"; // เปลี่ยนจาก home.html เป็น index.html
     });
-
   }
+});
+
+
