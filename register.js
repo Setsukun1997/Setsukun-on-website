e430d901236cef45a30d6ffe95a765d575fbfd8e
Setsukun1997 <tety12308@hotmail.com>
2025-09-05 13:19:34 +0700

    Update register.js

--- register.js
+++ register.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const gohomeBtn = document.getElementById("gohomeBtn");
   if (gohomeBtn) {
     gohomeBtn.addEventListener("click", () => {
-      window.location.href = "Register.html";
+      window.location.href = "home.html";
     });
+
   }
