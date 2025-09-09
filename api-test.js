77f00afd082332c3074b53b1acde5cae616590a6
Setsukun1997 <tety12308@hotmail.com>
2025-09-09 15:03:44 +0700

    Add files via upload

---
+++ api-test.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:3000/api/data')
+    .then(response => response.json())
+    .then(data => console.log(data));
+fetch('http://localhost:3000/api/data', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: 'Frontend User' })
+})
+    .then(response => response.json())
+    .then(data => console.log(data));
