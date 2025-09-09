39f4e353b842309919e099e29b984e1ec3dbce5d
Setsukun1997 <tety12308@hotmail.com>
2025-09-09 15:46:36 +0700

    Update api-test.js

--- api-test.js
+++ api-test.js
@@ -1,10 +1,11 @@
 fetch('http://localhost:3000/api/data')
     .then(response => response.json())
     .then(data => console.log(data));
-fetch('http://localhost:3000/api/data', {
+fetch('https://setsukun-on-website.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name: 'Frontend User' })
 })
     .then(response => response.json())
+
     .then(data => console.log(data));
