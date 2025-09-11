317c1c6c8cf5a9b8d5787e8dab7f079a61971465
Setsukun1997 <tety12308@hotmail.com>
2025-09-11 16:46:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.use(express.json());
 
 app.post("/api/login", (req, res) => {
@@ -16,6 +23,7 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
 
 
