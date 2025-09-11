5ea4fc6ffc826881f270bb816ed3cfc10556e19a
Setsukun1997 <tety12308@hotmail.com>
2025-09-11 16:43:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname)));
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  if (username === "admin" && password === "1234") {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, message: "รหัสผิด" });
+  }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.use(express.json());
 
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  if (username === 'admin' && password === '1234') {
-    res.json({ success: true });
-  } else {
-    res.json({ success: false, message: 'รหัสผิด' });
-  }
-});
 
 
 
