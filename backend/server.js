5f9e6eeb33c71a7fbb1628dc9837569279251d1f
Setsukun1997 <tety12308@hotmail.com>
2025-10-16 09:56:22 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,6 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
 
 
 
