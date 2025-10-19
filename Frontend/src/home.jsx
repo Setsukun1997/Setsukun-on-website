0989cd5aa48a3f45d6d4cef2849728eabd89e440
Setsukun1997 <tety12308@hotmail.com>
2025-10-19 20:32:54 +0700

    Update home.jsx

--- Frontend/src/home.jsx
+++ Frontend/src/home.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>ยินดีต้อนรับสู่ Setsukun Website 👋</h1>
-      <Link to="/login"><button>เข้าสู่ระบบ</button></Link>
+      <p>นี่คือจุดเริ่มต้นของส่วนหน้าแบบพัฒนาโดยโคดเนม!</p>
       <Link to="/register"><button>ลงทะเบียน</button></Link>
+      <Link to="/login"><button>เข้าสู่ระบบ</button></Link>
+      <Link to="/journey"><button>เส้นทางของเรา</button></Link>
     </div>
   );
 }
+
+export default Home;
