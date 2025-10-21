ff87b6e8822d161ba23d207c7a433a196585d566
Setsukun1997 <tety12308@hotmail.com>
2025-10-21 13:43:12 +0700

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -7,21 +6,16 @@ import Journey from './pages/Journey';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
-
 function App() {
   return (
-    <BrowserRouter>
-
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/journey" element={<Journey />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/journey" element={<Journey />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/resume" element={<Resume />} />
+    </Routes>
   );
 }
 
