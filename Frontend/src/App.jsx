8394382ddc623a9bf51835319284ec7fb79a4848
Setsukun1997 <tety12308@hotmail.com>
2025-10-22 13:45:44 +0700

    Fix duplicate import of Login component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
