a995987ef8e6bc667dcfcc6cdbdeea21f616f04e
Setsukun1997 <tety12308@hotmail.com>
2025-10-23 21:55:34 +0700

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Register from './pages/Register';
 import Journey from './pages/Journey';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import Register from './pages/Register';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/journey" element={<Journey />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/journey" element={<Journey />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
-
-export default App;
