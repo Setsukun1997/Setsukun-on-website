fe4c3e04c0a275ef1347a149243626d39123496a
Setsukun1997 <tety12308@hotmail.com>
2025-10-23 21:27:12 +0700

    Update main.js

--- Frontend/JS/main.js
+++ Frontend/JS/main.js
@@ -1,9 +1,12 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const goHomeBtn = document.getElementById("goHomeBtn");
-  if (goHomeBtn) {
-    goHomeBtn.addEventListener("click", () => {
-      window.location.href = "home.html";
-    });
-  }
-});
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
