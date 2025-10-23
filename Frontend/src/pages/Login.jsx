127fb95eab7ed172a7388027a857a67e5d9185ff
Setsukun1997 <tety12308@hotmail.com>
2025-10-23 21:06:47 +0700

    Delงete Frontend/src/pages/Login.jsx

--- Frontend/src/pages/Login.jsx
+++
@@ -1,5 +0,0 @@
-<div style={{ textAlign: 'center', paddingTop: '100px' }}>
-  <h1>ยินดีต้อนรับ</h1>
-  <button onClick={() => setShowLogin(true)}>เข้าสู่ระบบ</button>
-  <button onClick={() => setShowRegister(true)}>ลงทะเบียน</button>
-</div>
