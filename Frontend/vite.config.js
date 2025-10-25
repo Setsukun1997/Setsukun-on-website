b35da4dfd60d08ea50a891e7558aae3968d83a74
Setsukun1997 <tety12308@hotmail.com>
2025-10-25 13:16:04 +0700

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/', 
+  base: '/',
   build: {
     outDir: 'dist'
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'  
+        }
+      ]
+    })
+  ]
 });
