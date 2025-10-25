a1e57e6e6a1db3d991b21bfa1e5bc9aeafb15b97
Setsukun1997 <tety12308@hotmail.com>
2025-10-25 13:30:07 +0700

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Journey from './pages/Journey';
-import Contact from './pages/Contact';
 import Resume from './pages/Resume';
-import Register from './pages/Register';
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/journey" element={<Journey />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
